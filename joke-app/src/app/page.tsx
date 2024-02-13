617edf24507c018cd6d1d88233ae6593c805f62c
Liben Hailu <libenhailu04@gmail.com>
2024-02-13 09:29:51 +0300

    Update page.tsx

--- joke-app/src/app/page.tsx
+++ joke-app/src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client" 
 import React from 'react';
 import Head from 'next/head';
 import JokesDisplay from '../components/JokesDisplay'
@@ -6,15 +5,6 @@ import JokesDisplay from '../components/JokesDisplay'
 export default function JokesPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <Head>
-        <title>Jokes Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <header className="text-center mb-6">
-        
-      </header>
-
       <main>
         <JokesDisplay />
       </main>
